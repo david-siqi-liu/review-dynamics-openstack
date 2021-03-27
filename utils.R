241eb18a9ae3a8b3aad51078e47e3470a158755e
David Liu <davidsiqiliu@gmail.com>
2021-03-27 17:40:10 -0400

    added utils

---
+++ utils.R
@@ -0,0 +1,83 @@
+library(pROC)
+library(car)
+
+getREVar = function(model) {
+  #' Computes the variance of the random effect
+  #' 
+  #' @param model. The model
+  #' 
+  #' @return the variance of the random effect
+  
+  df = data.frame(VarCorr(model))
+  
+  return(round(df$vcov, digits=2))
+}
+
+getLR = function(model0, model1) {
+  #' Performs Log-Likelihood Ratio test on two models
+  #' 
+  #' @param model0. The unsaturaed/less-complex model
+  #' @param model1. The saturated/more-complex model
+  #' 
+  #' @return The -2 * log-likelihood ratio, differences in degrees-of-freedom, and the p-value
+  
+  # get log-likelihoods
+  L0 = logLik(model0)
+  L1 = logLik(model1)
+  # get -2 * log-likelihood ratio
+  LR = -2 * (as.numeric(L0) - as.numeric(L1))
+  # degrees-of-freedom difference
+  df = attr(L1, "df") - attr(L0, "df")
+  # get p-value from chi-square distribution
+  pval = pchisq(LR, df, lower.tail=FALSE)
+  
+  return(data.frame('lr' = LR,
+                    'diff_d.f.' = df,
+                    'pval' = pval))
+}
+
+getAUC = function(model, target_var) {
+  #' Computes AUC-ROC
+  #' 
+  #' @param model. The model
+  #' 
+  #' @return The AUC-ROC
+  
+  # predict
+  preds = predict(model)
+  # compute AUC-ROC
+  labels = model@frame[, target_var]
+  auc = auc(labels, preds)
+  
+  return(auc)
+}
+
+getVariableSigAndSign = function(model) {
+  #' Extracts Chisq value and computes the significance level and
+  #' the sign of the coefficients for each variable
+  #' 
+  #' @param model. The model
+  #' 
+  #' @return the Chisq value, significance level and sign for each variable
+  
+  # ANOVA
+  anova = data.frame(Anova(model, type = 3))
+  chisq_col = 'Chisq'
+  pval_col = 'Pr..Chisq.'
+  # Significance
+  anova$sig = "o"
+  anova$sig[anova[, pval_col] < 0.05] = "*"
+  anova$sig[anova[, pval_col] < 0.01] = "**"
+  anova$sig[anova[, pval_col] < 0.001] = "***"
+  # Coefficients
+  coefs = data.frame('val' = fixef(model))
+  # Sign
+  coefs$sign = ifelse(coefs$val < 0, "(-)", "(+)")
+  # Merge by row names/variables
+  df = merge(anova, coefs, by = 0, all = TRUE)
+  # Format
+  df$variable = df$Row.names
+  df[, chisq_col] = round(df[, chisq_col], digits=0)
+  
+  return(df[, c('variable', chisq_col, 'sig', 'sign')])
+}
